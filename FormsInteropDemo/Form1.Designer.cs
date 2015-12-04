2aaa9bb412670f3cccd99958961b03865adf28aa
Pavel Karpenko <pwkpwk@hotmail.com>
2015-12-03 18:35:22 -0800

    Tweak the main window's layout

--- FormsInteropDemo/Form1.Designer.cs
+++ FormsInteropDemo/Form1.Designer.cs
@@ -28,6 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Plop #2");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Plop #3");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Plop #1", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2});
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.splitter1 = new System.Windows.Forms.Splitter();
             this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
@@ -38,23 +43,32 @@
             this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
             this.treeView1.Location = new System.Drawing.Point(0, 0);
             this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(209, 361);
+            treeNode1.Name = "nodeChild1";
+            treeNode1.Text = "Plop #2";
+            treeNode2.Name = "nodeChild2";
+            treeNode2.Text = "Plop #3";
+            treeNode3.Checked = true;
+            treeNode3.Name = "nodeRoot";
+            treeNode3.Text = "Plop #1";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode3});
+            this.treeView1.Size = new System.Drawing.Size(264, 425);
             this.treeView1.TabIndex = 0;
             // 
             // splitter1
             // 
-            this.splitter1.Location = new System.Drawing.Point(209, 0);
+            this.splitter1.Location = new System.Drawing.Point(264, 0);
             this.splitter1.Name = "splitter1";
-            this.splitter1.Size = new System.Drawing.Size(3, 361);
+            this.splitter1.Size = new System.Drawing.Size(3, 425);
             this.splitter1.TabIndex = 1;
             this.splitter1.TabStop = false;
             // 
             // elementHost1
             // 
             this.elementHost1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.elementHost1.Location = new System.Drawing.Point(212, 0);
+            this.elementHost1.Location = new System.Drawing.Point(267, 0);
             this.elementHost1.Name = "elementHost1";
-            this.elementHost1.Size = new System.Drawing.Size(221, 361);
+            this.elementHost1.Size = new System.Drawing.Size(270, 425);
             this.elementHost1.TabIndex = 2;
             this.elementHost1.Text = "elementHost1";
             this.elementHost1.Child = null;
@@ -63,7 +77,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(433, 361);
+            this.ClientSize = new System.Drawing.Size(537, 425);
             this.Controls.Add(this.elementHost1);
             this.Controls.Add(this.splitter1);
             this.Controls.Add(this.treeView1);
